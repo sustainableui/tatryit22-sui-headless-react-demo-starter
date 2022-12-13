15d32d792165229e9e4e68a1f081e142135a977b
Samuel Pitoňák <sampittko@gmail.com>
2022-12-13 11:55:10 +0100

    refactor

--- src/components/elements/Section/Section.tsx
+++ src/components/elements/Section/Section.tsx
@@ -1,52 +1,32 @@
-import Headline from "./Headline";
-import PropTypes from "prop-types";
-import React from "react";
+import React, { ReactElement } from 'react';
 
-import backgroundPath from "../../../assets/images/backgrounds/pattern_elements.svg";
+import backgroundPath from '../../../assets/images/backgrounds/pattern_elements.svg';
 
-const Section = (props) => {
-  const defaultLayout = !props.customLayout ? (
-    <div className="grid grid-rows-8">
-      <div className="row-span-2 sm:row-span-1 xs:row-span-1">
-        <Headline text={props.title} />
-      </div>
-      <div
-        className={`row-span-6 sm:row-span-7 xs:row-span-7 xs:pl-7 xs:pr-7 sm:pl-20 sm:pr-20 md:pl-16 md:pr-16 lg:pl-20 lg:pr-20 xl:pl-24 xl:pr-24 ${
-          props.noBottom ? "" : "xs:pb-20 sm:pb-20 md:pb-32 lg:pb-36 xl:pb-40"
-        }`}
-      >
-        {props.children}
-      </div>
-    </div>
-  ) : null;
+interface SectionProps {
+  title: string;
+  children: ReactElement[];
+}
 
+function Section({ title, children }: SectionProps) {
   return (
     <div
-      className={`relative bg-scroll bg-no-repeat bg-cover ${
-        props.backgroundPositionClass ? props.backgroundPositionClass : "bg-center"
-      } ${props.fullscreen ? "min-h-screen" : ""}`}
-      style={{ backgroundImage: `url(${backgroundPath})` }}
-    >
-      {props.customLayout ? (
-        <React.Fragment>{props.children}</React.Fragment>
-      ) : (
-        <React.Fragment>{defaultLayout}</React.Fragment>
-      )}
+      className="relative bg-scroll bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundPath})` }}>
+      <div className="grid grid-rows-8">
+        <div className="row-span-2 sm:row-span-1 xs:row-span-1">
+          <div className="text-center break-normal p-16 md:pt-32 lg:pt-36 xl:pt-40">
+            <h1 className="font-rustico-regular text-5xl sm:text-4xl xs:text-4xl">{title}</h1>
+          </div>
+        </div>
+        <div
+          className={
+            'row-span-6 sm:row-span-7 xs:row-span-7 xs:pl-7 xs:pr-7 sm:pl-20 sm:pr-20 md:pl-16 md:pr-16 lg:pl-20 lg:pr-20 xl:pl-24 xl:pr-24 xs:pb-20 sm:pb-20 md:pb-32 lg:pb-36 xl:pb-40'
+          }>
+          {children}
+        </div>
+      </div>
     </div>
   );
-};
-
-Section.propTypes = {
-  customLayout: PropTypes.bool,
-  fullscreen: PropTypes.bool,
-  title: PropTypes.string,
-  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
-  noBottom: PropTypes.bool,
-  backgroundPositionClass: PropTypes.oneOf(["bg-top", "bg-bottom"])
-};
-
-Section.defaultProps = {
-  fullscreen: true,
 }
 
 export default Section;
