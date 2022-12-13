fbdcd298c45058c0f7076bc11f3052739e505a8e
Samuel Pitoňák <sampittko@gmail.com>
2022-12-13 10:18:05 +0100

    chore: updates proxy init log

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`);
+  console.log(`Proxy running on port ${PORT}`);
 });
 
 app.get('/api/grid-carbon-intensity', async (req, res) => {
